3893b69f59e802fef0226c337e9796881056fe95
reem <tarekreem950@gmail.com>
2023-03-17 11:57:42 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		if (c != 101 && c != 113)
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
