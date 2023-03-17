c8fa6c5ffb16b60c6f14c3ff403b99ded4daa8ca
reem <tarekreem950@gmail.com>
2023-03-17 12:34:26 -0700

    alphabet

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,11 @@ int main(void)
 	for (c = 48; c <= 57; c++)
 	{
 		putchar(c);
-		putchar(',');
-		putchar(' ');
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
