ef2fa2a62943485aa85743b1042443601d0b2b9b
reem <tarekreem950@gmail.com>
2023-03-17 13:03:57 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = a + 1; b <= 57; b++)
+		{
+			for (c = b + 1; c <= 57; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a != 55 || b != 56 || c != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
