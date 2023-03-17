38025595e62e35dfe824fd6eba892739bc00b0e0
reem <tarekreem950@gmail.com>
2023-03-17 12:25:42 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
