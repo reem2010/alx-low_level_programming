f13e8e6639414a5e76cb57a8783e7736430ae4af
reem <tarekreem950@gmail.com>
2023-03-17 18:16:36 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 			{
 				for (d = 48; d <= 57; d++)
 				{
-					if ((a != c || b != d) && ((d =  b) || (c >= a)))
+					if ((a != c || b != d) && ((d >  b) || (c >= a)))
 					{
 						putchar(a);
 						putchar(b);
