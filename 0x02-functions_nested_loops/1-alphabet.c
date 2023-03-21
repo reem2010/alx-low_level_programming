fdd5a38d63cda16d1addea5bde57e085d5127388
reem <tarekreem950@gmail.com>
2023-03-21 14:18:12 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet, in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
+}
