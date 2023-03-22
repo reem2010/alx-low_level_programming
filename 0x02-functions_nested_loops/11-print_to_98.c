7b33e079791a9b7b5acaae9f5d6dcab9c957b539
reem <tarekreem950@gmail.com>
2023-03-21 17:43:44 -0700

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,26 +14,24 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d",i);
+			
 			if (i != 98)
-			{
-				_putchar (',');
-				_putchar (' ');
-			}
+				printf("%d, ",i);
+			else
+				printf("%d",i);
 		}
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d",i);
+			
 			if (i != 98)
-			{
-				_putchar (',');
-				_putchar (' ');
-			}
+				printf("%d, ",i);
+			else
+				printf("%d",i);
 		}
-	}
+	}	
 	_putchar ('\n');
 }
 
