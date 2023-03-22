2907262c48bdcb57f7c7a6fbcea2e55733dbf07a
reem <tarekreem950@gmail.com>
2023-03-21 17:38:07 -0700

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - prints all natural numbers from n to 98
  *
@@ -9,15 +10,28 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		if ((i / 10) != 0)
-			_putchar('0' + (i / 10));
-		_putchar('0' + (i % 10));
-		if (i != 98)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar (',');
-			_putchar (' ');
+			printf("%d",i);
+			if (i != 98)
+			{
+				_putchar (',');
+				_putchar (' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d",i);
+			if (i != 98)
+			{
+				_putchar (',');
+				_putchar (' ');
+			}
 		}
 	}
 	_putchar ('\n');
