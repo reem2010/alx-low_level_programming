381f1c7ce1d3c5dd97ae8e03d9fb31d81f7830fd
reem <tarekreem950@gmail.com>
2023-03-23 08:04:22 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar (c);
+	}
+	_putchar ('\n');
+}
