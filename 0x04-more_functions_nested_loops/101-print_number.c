102c5d00c734bad816e3c9bb3b46fd39e5ae607e
reem <tarekreem950@gmail.com>
2023-03-24 06:21:56 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input
+ */
+
+void print_number(int n)
+{
+	int ind;
+	int dig;
+	int num;
+	int i;
+
+	num = n;
+	ind = 0;
+	if (n != 0)
+	{
+		while (n != 0)
+		{
+			ind = ind + 1;
+			num = num / 10;
+		}
+
+		for (i = 1; i < ind; i++)
+		{
+			dig = n / (10 * (ind - 1));
+			_putchar ('0' + dig);
+			n = n % (10 * (ind - 1));
+		}
+	}
+	_putchar ('0' + n);
+}
+
+
+
+
+
