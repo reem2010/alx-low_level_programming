10431dcc0281752c52ce8425f889fe38a9dbf056
reem <tarekreem950@gmail.com>
2023-03-27 13:23:07 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to string
+ */
+void rev_string(char *s)
+{
+	char *p;
+
+	p = s;
+	while (*p)
+	{
+		len = len + 1;
+		p = p + 1;
+	}
+	p = s + len - 1;
+	while ((p != s) && (s < p))
+	{
+		temp = *s;
+		*s = *p;
+		*p = temp;
+		s = s + 1;
+		p = p - 1;
+	}
+}
+
