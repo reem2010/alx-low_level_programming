60030b846af12620f1ebb5884ea8757d4d809c27
reem <tarekreem950@gmail.com>
2023-03-27 13:11:14 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * str - address of string
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar (*str);
+		str = str + 1;
+	}
+	_putchar ('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: pointer to string
+ */
+void print_rev(char *s)
+{
+	char *p;
+	int len;
+	int i;
+
+	p = s;
+	len = 0;
+	while (*p)
+	{
+		len = len + 1;
+		p = p + 1;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar (*(n + i));
+	}
+	_putchar ('\n');
+}
