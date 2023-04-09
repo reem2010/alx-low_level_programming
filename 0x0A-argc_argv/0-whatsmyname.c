b86bd14263455c85af6d8e9934bc9e323b9e54d8
reem <tarekreem950@gmail.com>
2023-04-09 11:36:25 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: the number of command arguments
+ * @argv: array containing a program command line arguments
+ * Return: Always 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+	printf ("%s\n", argv[0]);
+	return (0);
+}
