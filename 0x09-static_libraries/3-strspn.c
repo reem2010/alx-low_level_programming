0429d790923c4beec49744861f3874f109ebf42f
reem <tarekreem950@gmail.com>
2023-04-09 10:39:24 -0700

    task0

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn -  gets the length of a prefix substring
+ * @s: pointer to string
+ * @accept: bytes to search for
+ * Return: numer of pointers
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int num;
+	int j;
+
+	i = 0;
+	num = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] == ',' && s[i+1] == ' ')
+			break;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+				num++;
+		}
+	}
+	return (num);
+}
