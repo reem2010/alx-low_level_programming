b7534949d206fdf695ade8fb7e99b0b828766ba1
reem <tarekreem950@gmail.com>
2023-04-09 12:20:49 -0700

    task5

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strpbrk -  searches a string for any of a set of bytes
+ * @s: input string
+ * @accept: string to compare
+ * Return:  pointer to the byte in s that matches one of the bytes in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+
+
+	i = 0;
+	return (s + i);
+}
+
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strstr -  locates a substring
+ * @haystack: input string
+ * @needle: string to be compared
+ * Return: first occurence
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	i = 0;
+	return (haystack + i)
+}
