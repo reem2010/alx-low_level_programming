8e6a6e430c101123943c8138ccdf12920cfc8f12
reem <tarekreem950@gmail.com>
2023-04-08 21:09:57 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * *dest: pointer for destination
+ * *src: pointersource
+ * n: number of bytes
+ * Return: pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest)
+}
