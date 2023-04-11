a5c80e2b4a0019b45e465c93e46c27904c794f38
reem <tarekreem950@gmail.com>
2023-04-11 11:16:07 -0700

    task 0 and 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	p = (char *)malloc(sizeof(char) * size);
 	if (p == NULL)
 		return (NULL);
