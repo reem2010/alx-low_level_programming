9d6a1f7fad13783bcc688d88816fef1d7a94d025
reem <tarekreem950@gmail.com>
2023-04-13 11:19:00 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < size1; i++)
 		p[i] = s1[i];
 	for (i = 0; i < n; i++)
-		p[i + size1] = s2[i];	
+		p[i + size1] = s2[i];
 	p[i + size1] = '\0';
 	return (p);
 }
