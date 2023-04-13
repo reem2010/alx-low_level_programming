4769989f42f11493f0968859d1e57a1b3aa12d10
reem <tarekreem950@gmail.com>
2023-04-13 11:36:47 -0700

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,13 +9,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	unsigned int i;
 
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = '0';
 	return (p);
 }
 	
