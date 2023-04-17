0c7973de91d381288d7be8d2396b79f9a12ba146
reem <tarekreem950@gmail.com>
2023-04-17 13:55:31 -0700

    task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
+	if (*(name) != '\0')
+		(*d).name = name;
 	(*d).age = age;
-	(*d).owner = owner;
+	if (*(name) != '\0')
+		(*d).owner = owner;
 }
