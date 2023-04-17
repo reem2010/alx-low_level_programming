aa889e0912d04c7911f923fd1eda98062a4ee521
reem <tarekreem950@gmail.com>
2023-04-17 14:59:41 -0700

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,12 +11,13 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+
 	dog = malloc(sizeof(dog));
 	if (dog == NULL)
 		return (NULL);
-	*(dog).name = name;
-	*(dog).age = age;
-	*(dog).owner = owner;
-	return(dog);
+	(*dog).name = name;
+	(*dog).age = age;
+	(*dog).owner = owner;
+	return (dog);
 }
 
