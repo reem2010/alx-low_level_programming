b887e3100adddd7809631c98b6e2170e23f63f0d
reem <tarekreem950@gmail.com>
2023-04-16 18:03:04 -0700

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: the string
+ */
+void _puts_recursion(char *s)
+{
+	if (*(s) != NULL)
+	{
+		_putchar(*(s));
+		_puts_recursion(*(s+1));
+	}
+}
