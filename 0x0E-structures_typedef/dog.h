a4bd2ef46128f77b26877101267c6de9f9f77f00
reem <tarekreem950@gmail.com>
2023-04-17 12:12:16 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
-#define DOH_H
+#define DOG_H
+/**
+ * struct dog - define new type dog
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the owner of the dog
+ * Description: this new tybe give information about
+ * the dog name ,age and owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 #endif
