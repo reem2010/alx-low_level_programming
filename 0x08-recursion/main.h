e0c99e9ba950f3eda621bf6aed278ad43619f4a4
reem <tarekreem950@gmail.com>
2023-04-16 18:10:25 -0700

    task0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char _putchar (char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -5,4 +8,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int is_prime_number(int n);`
+int is_prime_number(int n);
+
+#endif
