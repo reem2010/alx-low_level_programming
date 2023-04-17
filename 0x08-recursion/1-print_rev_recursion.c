845f911b3e7a47b8e87314fe61b7524b05ddfbb1
reem <tarekreem950@gmail.com>
2023-04-16 18:23:52 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,8 +5,8 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*(s + 1) != '\0')
-		_print_rev_recursion(s +1);
+	if ((*(s + 1) != '\0') && (*s != ''))
+		_print_rev_recursion(s + 1);
 	_putchar (*s);
 }
 
