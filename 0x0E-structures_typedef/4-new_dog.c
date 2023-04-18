f6e65d6c2507211a41c5cd883c2ad485e52ec1c9
reem <tarekreem950@gmail.com>
2023-04-17 17:25:02 -0700

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,11 +12,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	dog = malloc(sizeof(name) + sizeof(float) + sizeof(owner));
+	dog = malloc(sizeof(dog_t *) * 3);
 	if (dog == NULL)
+		return (NULL);
+	(*dog).name = malloc(sizeof(name));
+	if ((*dog).name == NULL)
 		return (NULL);
 	(*dog).name = name;
 	(*dog).age = age;
+	(*dog).owner = malloc(sizeof(owner));
+	if ((*dog).owner == NULL)
+		return (NULL);
 	(*dog).owner = owner;
 	return (dog);
 }
