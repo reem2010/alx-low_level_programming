fe535f6393c6e72e756ce8858be4135dea29399b
reem <tarekreem950@gmail.com>
2023-04-17 18:11:07 -0700

    task3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,4 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
-#define ABS(x) (((((x) < 0) * -2) + 1) * x)
-
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) (((((int)((x) < 0)) * -2) + 1) * x)
 #endif
