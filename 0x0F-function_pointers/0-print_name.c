d0610737988c06dac4c6dcc9056090dc2bacfc16
reem <tarekreem950@gmail.com>
2023-04-19 11:22:42 -0700

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - prints a name.
  * @name: the name
@@ -6,5 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
