509e9975eeb137ea68418b82ade170e3c0ac3a49
reem <tarekreem950@gmail.com>
2023-04-19 12:25:32 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,15 +18,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (-1);
 		for (i = 0; i < size; i++)
 		{
-			found = p(array[i]);
+			found = cmp(array[i]);
 			if (found)
 				return (i);
 		}
 		if (!found)
 			return (-1);
 	}
-	else
-		return (-1);
+	return (-1);
 }
 
 
