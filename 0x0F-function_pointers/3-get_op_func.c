c1461c242afb755c1850d6630c957d843fd58a62
reem <tarekreem950@gmail.com>
2023-04-19 15:32:19 -0700

    task3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,7 +19,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while ((i < 6) && (strlen(s)) == 1)
+	while ((i < 5) && (s != NULL) && (strlen(s) == 1))
 	{
 		if ((ops[i].op)[0] == s[0])
 			return (ops[i].f);
