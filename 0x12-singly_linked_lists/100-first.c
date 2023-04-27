69be030490b492b2eac01a65c4142782b9c2ce13
reem <tarekreem950@gmail.com>
2023-04-27 16:00:48 -0700

    task5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * print - this funstion is executed first
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
