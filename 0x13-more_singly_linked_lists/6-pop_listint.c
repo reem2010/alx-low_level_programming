15cdf74b4b5a950fbb999ee45d44961fbb792bfa
reem <tarekreem950@gmail.com>
2023-04-30 16:42:09 -0700

    task6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,3 +1,21 @@
 #include "lists.h"
 /**
- *
+ * pop_listint - deletes and return the head node
+ * @head: head of list
+ * Return: return the head node
+ */
+int pop_listint(listint_t **head)
+{
+	int res = 0;
+	listint_t *temp;
+
+	if (*head)
+	{
+		temp = (*head)->next;
+		res = (*head)->n;
+		free(*head);
+		(*head) = temp;
+	}
+	return (res);
+}
+
