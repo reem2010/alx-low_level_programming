714986109f1b590d0079d1d82fcbd674f174a0f8
reem <tarekreem950@gmail.com>
2023-05-08 19:31:04 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,12 +25,15 @@ int main(int argc, char *argv[])
 	s = malloc(1024);
 	if (s)
 	{
-		ind = read(fd, s, 1024);
-		if (ind == -1)
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
-		ind1 = write(fd1, s, ind);
-		if (ind1 == -1)
-			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		while (ind > 0)
+		{
+			ind = read(fd, s, 1024);
+			if (ind == -1)
+				dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			ind1 = write(fd1, s, ind);
+			if (ind1 == -1)
+				dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
 	}
 	ind = close(fd);
 	if (ind == -1)
