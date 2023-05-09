a61e2b1ac01a5f2cd2c9ac138e1eae3b5861bb7a
reem <tarekreem950@gmail.com>
2023-05-09 16:29:05 -0700

    delete test files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	while (ind)
 	{
 		ind = read(fd, s, 1024);
