5cf5206841749403fe5ce206db2064ab899df770
reem <tarekreem950@gmail.com>
2023-05-08 19:28:16 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
-	fd1 = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
+	fd1 = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0664 | S_IWGRP);
 	if (fd1 == -1)
 		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	fd = open(argv[1], O_RDONLY);
