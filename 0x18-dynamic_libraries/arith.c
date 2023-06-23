e43947650d34a059cc4c3de252f6bc37008a983b
reem <tarekreem950@gmail.com>
2023-06-23 07:30:59 -0700

    task2

--- 0x18-dynamic_libraries/arith.c
+++ 0x18-dynamic_libraries/arith.c
@@ -1,4 +1,4 @@
-#include "main.h"
+
 /**
  * add - add two numbers
  * @x: first numbrt
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,10 +21,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int mod(int x, int y);
-int sub(int x, int y);
-int add(int x, int y);
-int mul(int x, int y);
-int div(int x, int y);
+
 
 #endif
