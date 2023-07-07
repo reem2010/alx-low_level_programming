a241fb866b46fbdce91b2fdc7a1f76ca139d5f61
reem <tarekreem950@gmail.com>
2023-07-06 17:30:08 -0700

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
-	if (size == 0)
-		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
-	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!table->array)
+	if (size != 0)
 	{
-		free(table);
-		return (NULL);
+		table->array = malloc(sizeof(hash_node_t *) * size);
+		if (!table->array)
+		{
+			free(table);
+			return (NULL);
+		}
 	}
 	return (table);
 }
