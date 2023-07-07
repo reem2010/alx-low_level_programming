dbfcac699440624123c6728b9a870121186e2991
reem <tarekreem950@gmail.com>
2023-07-07 11:04:35 -0700

    task0

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,4 +1,14 @@
 #include "hash_tables.h"
 /**
  * hash_table_print - prints a hash table.
- * @
+ * @ht: is the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	
+	if (!ht)
+		return;
+
+
+}
