8e232b25f63809eed22f8713ad4c661b849401bc
reem <tarekreem950@gmail.com>
2023-08-23 16:31:06 -0700

    task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,10 +6,10 @@ def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     perimeter = 0
     if len(grid) > 100:
-        return
+        return perimeter
     for i in range(len(grid)):
         if len(grid[i]) > 100:
-            return
+            return perimeter
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if grid[i + 1][j] == 0:
